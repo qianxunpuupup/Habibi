45eb12f09103bdc6e1df4edfe472be99eea2faa8
qianxunpuupup <wangh9696@163.com>
2026-09-01 21:21:44 +0800

    修改打版目录

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,26 @@ import { fileURLToPath, URL } from "node:url";
 export default defineConfig({
   base: "/Habibi/",
   server: {
-    port: 9088,
+    port: 9090,
+    open: true,
   },
 
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  build: {
+    target: "es2015",
+    outDir: "docs",
+    chunkSizeWarningLimit: 2000,
+
+    dynamicImportVarsOptions: {
+      warnOnError: true,
+      exclude: [],
+      include: ["src/views/**/*.vue"],
     },
   },
 });
