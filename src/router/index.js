b19a3bacfeac8f1d6c799de7373a56a08b378811
qianxunpuupup <wangh9696@163.com>
2026-09-01 20:44:38 +0800

    添加路由信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  }
-]
+    path: "/Habibi",
+    name: "home",
+    component: HomeView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
